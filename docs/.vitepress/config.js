0ae552856dd38f7fb959d542be90c918b0b96cc5
Nuoya Wang <wang.noah@gmail.com>
2023-02-18 23:33:14 +1300

    dist

--- docs/.vitepress/config.js
+++
@@ -1,4 +0,0 @@
-export default {
-  title: "Noah's AI",
-  description: "Noah's personal site"
-}
--- docs/blog_index.js
+++
@@ -1,1 +0,0 @@
-export default [{"title":"Welcome to my blog!","date":"11 Feb 2023","description":"My first post ever.","path":"./blog/1.html"}]
