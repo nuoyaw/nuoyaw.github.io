733850dd5dca6b5490dd3b5c097142f3fb4c090d
Nuoya Wang <wang.noah@gmail.com>
2023-04-04 22:50:37 +1200

    Add deploy script

---
+++ deploy.mjs
@@ -0,0 +1,28 @@
+/* eslint-disable no-console */
+// const execa = require("execa");
+// const fs = require("fs");
+
+import {execa} from 'execa'
+
+(async () => {
+  try {
+    await execa("git", ["checkout", "--orphan", "gh-pages"]);
+    // eslint-disable-next-line no-console
+    console.log("Building started...");
+    await execa("yarn", ["build"]);
+    // Understand if it's dist or build folder
+    const folderName = "docs/.vitepress/dist";
+    await execa("echo", ['"noahs.ai"', ">>", `${folderName}/CNAME`]);
+    await execa("git", ["--work-tree", folderName, "add", "--all"]);
+    await execa("git", ["--work-tree", folderName, "commit", "-m", "gh-pages"]);
+    console.log("Pushing to gh-pages...");
+    await execa("git", ["push", "origin", "HEAD:gh-pages", "--force"]);
+    await execa("git", ["checkout", "-f", "main"]);
+    await execa("git", ["branch", "-D", "gh-pages"]);
+    console.log("Successfully deployed, check your settings");
+  } catch (e) {
+    // eslint-disable-next-line no-console
+    console.log(e.message);
+    process.exit(1);
+  }
+})();
